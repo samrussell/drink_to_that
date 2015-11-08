0d38201ccc6a6dbfb87f4491e2b6f2ba617f7a9f
Sam Russell <sam.h.russell@gmail.com>
2015-11-08 14:41:40 +1300

    changes icons in the interactions screen

--- drink_to_that/content_script.js
+++ drink_to_that/content_script.js
@@ -2,6 +2,7 @@ var beerURL = chrome.extension.getURL("/images/drink_to_that.png");
 
 var styleTag = document.createElement("style");
 styleTag.type = "text/css";
-styleTag.appendChild(document.createTextNode("div.HeartAnimation { background-image: url(" + beerURL + ") }"));
+styleTag.appendChild(document.createTextNode("div.HeartAnimation { background-image: url(" + beerURL + ") }\n"));
+styleTag.appendChild(document.createTextNode("span.Icon--heartBadge:before { content: \"\\1f37a\" }\n"));
 
 document.head.appendChild(styleTag);
